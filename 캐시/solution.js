b6367e769f8b2bb3d63946a4960d38fbf69791b7
허도영 <hdy@doldolma.com>
2022-01-17 11:22:48 +0900

    캐시 리펙토링

--- 캐시/solution.js
+++ 캐시/solution.js
@@ -1,3 +1,4 @@
+// https://programmers.co.kr/learn/courses/30/lessons/17680
 function solution(cacheSize, cities) {
   let time = 0;
   const cache = [];
@@ -5,18 +6,15 @@ function solution(cacheSize, cities) {
   cities.forEach(city => {
       city = city.toLowerCase();
       const isIn = cache.includes(city);
-      if(isIn) time += 1;
-      else time += 5;
+      time += isIn ? 1 : 5;
       
       // cache 조정
       cache.push(city);
-      if(cache.length > cacheSize) {
-          if(isIn) cache.splice(cache.indexOf(city), 1);
-          else cache.shift();
-      }
+      if(isIn) cache.splice(cache.indexOf(city), 1);
+      else if(cache.length > cacheSize) cache.shift();
   });
   return time;
 }
 
-
-console.log(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+console.log(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]));
+// 50
