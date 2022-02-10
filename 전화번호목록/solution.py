6117ed31ddc6dbb5fd11e87f4c22a42e4211a204
허도영 <hdy@doldolma.com>
2022-02-10 12:00:00 +0900

    전화번호목록

---
+++ 전화번호목록/solution.py
@@ -0,0 +1,14 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42577
+
+def solution(phone_book):
+    phone_book.sort()
+
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+
+    return True
+
+
+if __name__ == '__main__':
+  print(solution(["119", "97674223", "1195524421"])) # False
